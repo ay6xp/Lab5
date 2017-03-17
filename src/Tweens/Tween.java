e48f8b3750da61ebfc36c316af1ed68a95795e50
Brigham Bangerter <bb3uc@virginia.edu>
2017-03-17 19:57:57 -0400

    adjustments to tweening

--- src/Tweens/Tween.java
+++ src/Tweens/Tween.java
@@ -1,25 +1,48 @@
 package Tweens;
 
 import edu.virginia.engine.display.DisplayObject;
+import edu.virginia.engine.display.Sprite;
 
 public class Tween {
-	Tween(DisplayObject object){
-		
-	}
-	Tween(DisplayObject object, TweenTransitions transition){
-	
-	}
-	public void animate(TweenableParams fieldToAnimate, double startVal, double endVal, double time){
-		
-	}
-	public void update(){
-		
-	}
-	public boolean isComplete(){
-		return true;
-	}
-	public void setValue(TweenableParams param, double value){
-		
-	}
-	
+    TweenParam tweenParam;
+    DisplayObject sprite;
+    TweenTransitions tweenTransitions;
+    double percentDone = 0;
+
+    Tween(DisplayObject object) {
+
+    }
+
+    public Tween(DisplayObject object, TweenTransitions transition) {
+        sprite = object;
+        tweenTransitions = transition;
+    }
+
+    public void animate(TweenableParams fieldToAnimate, double startVal, double endVal, double time) {
+        tweenParam = new TweenParam(fieldToAnimate, startVal, endVal, time);
+    }
+
+    public void update() {
+        if (tweenParam.getParem() == TweenableParams.Y) {
+            sprite.setPositionY(sprite.getPositionY() + tweenTransitions.applyTransition(tweenParam.startVal, tweenParam.endVal, tweenParam.time, percentDone));
+            percentDone+=1;
+            System.out.println(sprite.getPositionY());
+        }
+    }
+
+
+    public boolean isComplete() {
+        if (percentDone >= 100){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void setValue(TweenableParams param, double value) {
+
+    }
+
+
 }
--- src/Tweens/TweenTransitions.java
+++ src/Tweens/TweenTransitions.java
@@ -2,12 +2,35 @@ package Tweens;
 
 public class TweenTransitions {
 
-	public void applyTransition(double percentDone){
-		
-	}
-	public void easeInOut(double percentDone){
-		
-	}
-	
-	
+    private int Time;
+    private String transitionType;
+
+    public TweenTransitions(String string) {
+        transitionType = string;
+    }
+
+    public double applyTransition(double startVal, double endVal, double time, double percentDone) {
+        if (transitionType == "linearTransition") {
+            return linearTransition(startVal, endVal, time, percentDone);
+        } else if (transitionType == "easeInOut") {
+            return easeInOut(startVal, endVal, time, percentDone);
+        }
+        throw new RuntimeException("no valid transition Type");
+    }
+
+    private double easeInOut(double startVal, double endVal, double time, double percentDone) {
+            return 0.0;
+    }
+
+    public double linearTransition(double startVal, double endVal, double time, double percentDone) {
+        return startVal+1*percentDone;
+    }
+
+    private void intoTransition(double startVal, double endVal, double time, double percentDone) {
+
+    }
+
+    public String getTransitionType() {
+        return transitionType;
+    }
 }
